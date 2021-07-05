1576830752f379f11dffbceffe13df1238c34f13
TranRepo <saey1079@gmail.com>
2021-07-05 22:46:49 +0700

    Latest for first day

--- App.py
+++ App.py
@@ -1,16 +1,13 @@
 from tkinter import *
+import tkinter as tk
 
 class Application:
     def __init__(self):
-        self.Screen = Tk()
+        self.Screen = Tk(state = tk.NORMAL)
         HeadFrame = Frame(self.Screen, height= 300 , width= 1360 )
-        canvas = Canvas(
-                    self.Screen,
-                    height=300,
-                    width=1360,
-                    bg="#3399ff"
-                    )
+        canvas = Canvas(self.Screen, height=300, width=1360, bg="#3399ff")
         canvas.pack()
+        
         rectangleMenu = canvas.Rectangle(0,0,300,1360,fill='#3399ff')
         HeadFrame.pack()
 
@@ -20,3 +17,5 @@ class Application:
             screen.mainloop()
 
 
+AppWindow = Application()
+AppWindow.show()
