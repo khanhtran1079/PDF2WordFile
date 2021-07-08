0718d2698b8998dc517754e9452805f96d4c84e3
TranRepo <saey1079@gmail.com>
2021-07-09 05:33:35 +0700

    change to c#

---
+++ PDF2Word/PDF2Word/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace PDF2Word
+{
+    partial class App
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelInput = new System.Windows.Forms.Panel();
+            this.panelOutput = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnOutputpath = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelInput.SuspendLayout();
+            this.panelOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelInput
+            // 
+            this.panelInput.Controls.Add(this.btnInput);
+            this.panelInput.Controls.Add(this.txtInput);
+            this.panelInput.Controls.Add(this.label1);
+            this.panelInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelInput.Location = new System.Drawing.Point(0, 0);
+            this.panelInput.Name = "panelInput";
+            this.panelInput.Size = new System.Drawing.Size(1098, 131);
+            this.panelInput.TabIndex = 0;
+            // 
+            // panelOutput
+            // 
+            this.panelOutput.Controls.Add(this.btnOutputpath);
+            this.panelOutput.Controls.Add(this.textBox1);
+            this.panelOutput.Controls.Add(this.label2);
+            this.panelOutput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelOutput.Location = new System.Drawing.Point(0, 131);
+            this.panelOutput.Name = "panelOutput";
+            this.panelOutput.Size = new System.Drawing.Size(1098, 158);
+            this.panelOutput.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label1.Location = new System.Drawing.Point(103, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File:";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtInput.Location = new System.Drawing.Point(240, 58);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(452, 27);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btnInput.Location = new System.Drawing.Point(747, 61);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(186, 36);
+            this.btnInput.TabIndex = 2;
+            this.btnInput.Text = "Choose file";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnOutputpath
+            // 
+            this.btnOutputpath.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.btnOutputpath.Location = new System.Drawing.Point(747, 69);
+            this.btnOutputpath.Name = "btnOutputpath";
+            this.btnOutputpath.Size = new System.Drawing.Size(186, 36);
+            this.btnOutputpath.TabIndex = 5;
+            this.btnOutputpath.Text = "Direct";
+            this.btnOutputpath.UseVisualStyleBackColor = true;
+            this.btnOutputpath.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox1.Location = new System.Drawing.Point(240, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(452, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label2.Location = new System.Drawing.Point(103, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output path:";
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1098, 456);
+            this.Controls.Add(this.panelOutput);
+            this.Controls.Add(this.panelInput);
+            this.Name = "App";
+            this.Text = "Form1";
+            this.panelInput.ResumeLayout(false);
+            this.panelInput.PerformLayout();
+            this.panelOutput.ResumeLayout(false);
+            this.panelOutput.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelInput;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelOutput;
+        private System.Windows.Forms.Button btnOutputpath;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PDF2Word/PDF2Word/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF2Word
+{
+    public partial class App : Form
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog()) { Filter = "PDF files | *.pdf" }
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtInput.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+    }
+}
